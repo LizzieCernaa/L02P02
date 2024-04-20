0fe86757db0383f56be3ca5d69413121b0e3182a
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2024-04-20 09:27:37 -0600

    Gm

--- L02P022021OC651/Pages/cont/guardar.cs
+++ L02P022021OC651/Pages/cont/guardar.cs
@@ -2,5 +2,49 @@
 {
     public class guardar
     {
+        <form id = "formulario_usuario" >
+    < !--Resto de campos del formulario -->
+    <input type = "button" value="Guardar" onclick="guardarUsuario()">
+</form>
+
+            <script>
+function guardarUsuario()
+        {
+            var nombre = document.getElementById('nombre').value;
+            var correo = document.getElementById('correo').value;
+            var clienteExistente = document.getElementById('cliente_existente').value;
+
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'guardar_usuario.php', true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200)
+                {
+
+                    refrescarListadoClientes();
+                }
+            };
+            xhr.send('nombre=' + nombre + '&correo=' + correo + '&cliente_existente=' + clienteExistente);
+        }
+
+        function refrescarListadoClientes()
+        {
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'obtener_clientes.php', true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200)
+                {
+
+                    document.getElementById('clientes_existente').innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send();
+        }
+</script>
+
+
+
     }
 }
